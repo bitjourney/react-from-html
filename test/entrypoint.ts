92a3d41fc7154cd80c5cfa42899f760d8cf899f5
FUJI Goro (gfx) <gfuji@cpan.org>
2019-02-28 21:41:36 +0900

    more tests

--- test/entrypoint.ts
+++ test/entrypoint.ts
@@ -1,25 +1,32 @@
 import React from "react";
 
-export const ReactFromHtml = (() => {
-  if (process.env.TEST_DIST) {
-    return require("..").ReactFromHtml;
-  } else {
-    return require("../src").ReactFromHtml;
-  }
-})();
+function missing(name: string) {
+  throw new Error(`Missing ${name}: make clean && make build will fix it`);
+}
 
-export const RawHtml: React.FC<{ html: string }> = (() => {
-  if (process.env.TEST_DIST) {
-    return require("..").RawHtml;
-  } else {
-    return require("../src").RawHtml;
-  }
-})();
+export const ReactFromHtml =
+  (() => {
+    if (process.env.TEST_DIST) {
+      return require("..").ReactFromHtml;
+    } else {
+      return require("../src").ReactFromHtml;
+    }
+  })() || missing("ReactFromHtml");
 
-export const DOMParserContext: React.Context<DOMParser> = (() => {
-  if (process.env.TEST_DIST) {
-    return require("..").DOMParserContext;
-  } else {
-    return require("../src").DOMParserContext;
-  }
-})();
+export const RawHtml: React.FC<{ html: string }> =
+  (() => {
+    if (process.env.TEST_DIST) {
+      return require("..").RawHtml;
+    } else {
+      return require("../src").RawHtml;
+    }
+  })() || missing("RawHtml");
+
+export const DOMParserContext: React.Context<DOMParser> =
+  (() => {
+    if (process.env.TEST_DIST) {
+      return require("..").DOMParserContext;
+    } else {
+      return require("../src").DOMParserContext;
+    }
+  })() || missing("DOMParserContext");
